ec8c320a1c9c546db2f6f0886bc0d9bc17cd0b31
jayde-code <juhnmayer@gmail.com>
2021-05-17 17:45:38 +0900

    final commit

---
+++ encapsulation/bmi.py
@@ -0,0 +1,12 @@
+class BodyMaxIndex:
+    def setData(self, weight, height):
+        self.weight = weight
+        self.height = height
+    def getBmi(self):
+        return self.weight / (self.height/100 * self.height/100)
+
+if __name__ == '__main__':
+    b = BodyMaxIndex()
+    b.setData(80, 179)
+    print(f'입력된 사용자의 BMI 지수는 {format(b.getBmi(), ".2f")} 입니다.')
+    print('18.5 이하는 저체중, 18.5~25의 경우 정상, 25~30의 경우 과체중입니다.')
