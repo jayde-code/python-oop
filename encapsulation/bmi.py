5f72a5bcc59c17585dfb2b276578e5683524771f
jayde-code <juhnmayer@gmail.com>
2021-05-18 17:13:01 +0900

    7th commit

--- encapsulation/bmi.py
+++ encapsulation/bmi.py
@@ -7,6 +7,14 @@ class Bmi(object):
         self.h = height
         self.w = weight
 
+    '''
+    고도 비만 : 35 이상
+    중(重)도 비만 (2단계 비만) : 30 - 34.9
+    경도 비만 (1단계 비만) : 25 - 29.9
+    과체중 : 23 - 24.9
+    정상 : 18.5 - 22.9
+    저체중 : 18.5 미만
+    '''
     def get_bmi(self):
         return self.w / self.h ** 2 * 10000
 
@@ -30,8 +38,7 @@ class Bmi(object):
 
     @staticmethod
     def main():
-        b = Bmi(int(input('키? ')), int(input('몸무게? ')))
-
+        b = Bmi(int(input('키 입력(cm)? ')), int(input('몸무게 입력(kg)? ')))
         print(f'입력하신 사용자의 BMI 지수는 {round(b.get_bmi(), 2)} 이며,')
         print(f'{b.get_status()} 입니다')
 
