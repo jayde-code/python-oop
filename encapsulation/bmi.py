654a27ba1dde84bdc441a0d613d4e968fb46ac38
jayde-code <juhnmayer@gmail.com>
2021-05-18 11:23:12 +0900

    second commit

--- encapsulation/bmi.py
+++ encapsulation/bmi.py
@@ -1,15 +1,19 @@
 class BodyMaxIndex:
-    def setData(self, weight, height):
+
+    def __init__(self, weight, height):
         self.weight = weight
         self.height = height
+
+    # def setData(self, weight, height):
+    #     self.weight = weight
+    #     self.height = height
     def getBmi(self):
         return self.weight / (self.height/100 * self.height/100)
 
 if __name__ == '__main__':
-    b = BodyMaxIndex()
-    b.setData(80, 179)
+    b = BodyMaxIndex(78, 179)
+    # b.setData(80, 179)
     print('==============================================================')
     print(f'입력된 사용자의 BMI 지수는 {format(b.getBmi(), ".2f")} 입니다.')
     print('18.5 이하는 저체중, 18.5~25의 경우 정상, 25~30의 경우 과체중입니다.')
-    print('TEST')
     print('==============================================================')
---
+++ encapsulation/calculator-constructor.py
@@ -0,0 +1,33 @@
+class Calculator:
+
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+    # def setdata(self, first, second):
+    #     self.first = first
+    #     self.second = second
+
+    def add(self):
+        return c.first + c.second
+
+    def sub(self):
+        return c.first - c.second
+
+    def mul(self):
+        return c.first * c.second
+
+    def div(self):
+        if c.first >= c.second:
+            return c.first / c.second
+        else:
+            return c.second / c.first
+
+if __name__ == '__main__':
+    c = Calculator(2, 7)
+    # c.setdata(2, 7)
+    print(id(c))
+    print(c.add())
+    print(c.sub())
+    print(c.mul())
+    print(c.div())
