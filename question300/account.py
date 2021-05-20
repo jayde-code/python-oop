fee8533d530728cd1df887834fd125309d983974
jayde-code <juhnmayer@gmail.com>
2021-05-20 09:41:04 +0900

    271~280 commit

--- question300/account.py
+++ question300/account.py
@@ -9,6 +9,7 @@ class Account(object):
         self.name = name
         self.balance = balance
         self.deposit_count = 0
+        self.deposit_interest = 0
         self.deposit_log = []
         self.withdraw_log = []
         self.bank = "SC은행"
@@ -26,14 +27,21 @@ class Account(object):
     def deposit(self, dep):
         if dep >= 1:
             self.balance += dep
+            self.deposit_count += 1
             self.deposit_log.append(dep)
             print(f'{self.name} 님의 계좌에 {dep}원이 입금되어 잔액은 {self.balance}원 입니다.')
+            if self.deposit_count % 5 == 0:
+                self.deposit_interest = self.balance * 0.01
+                self.balance += self.deposit_interest
+                print(f'{self.deposit_interest}원의 이자가 발생했습니다.')
+                print(f'현재 잔액 : {self.balance}')
         else:
             print('1원 이상부터 입금할 수 있습니다.')
 
     def withdraw(self, wit):
         if wit <= self.balance:
             self.balance -= wit
+            self.withdraw_log.append(wit)
             print(f'{self.name} 님의 계좌에서 {wit}원이 출금되어 잔액은 {self.balance}원 입니다.')
         else:
             print('잔액이 부족합니다.')
@@ -45,13 +53,17 @@ class Account(object):
         print(f'잔액 : {self.balance}')
 
     def deposit_history(self):
+        for amount in self.deposit_log:
+            print(f'{amount}원 입금')
+
+    def withdraw_history(self):
         for amount in self.withdraw_log:
-            print(amount)
+            print(f'{amount}원 출금')
 
 
     @staticmethod
     def main():
-        a = Account("ㅅㅈ", 50000)
+        a = Account("ㅅㅈ", 500000)
         b = Account("ㅂㅂ", 10000)
         c = Account("ㅎㅎ", 25000)
         acc_list = [a, b, c]
@@ -59,16 +71,24 @@ class Account(object):
         # print(b.name, b.acc_no, b.balance)
         # print(f'개설된 계좌수 : {a.get_account_num()}')
         # b.deposit(500)
-        a.withdraw(10000)
+        a.display_info()
+        a.deposit(10000)
+        a.deposit(20000)
+        a.deposit(30000)
+        a.deposit(50000)
+        a.deposit(40000)
+
+        a.withdraw(50000)
+        a.withdraw(30000)
         a.withdraw(20000)
-        a.deposit_history()
-        # a.display_info()
+        a.withdraw(60000)
         # b.display_info()
 
         # for i in acc_list:
         #     if i.balance >= 20000:
         #         i.display_info()
-
+        a.deposit_history()
+        a.withdraw_history()
 
 
 Account.main()
